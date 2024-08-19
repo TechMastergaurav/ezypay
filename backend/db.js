5da82c1128bbe2ffdc3096b0929e4c529440eba4
TechMastergaurav <mtechgaurav03@gmail.com>
2024-08-19 11:39:25 +0530

    newpaymentapp

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose");
+
+mongoose.connect("mongodb+srv://mtechgaurav03:iB4QB0XeDhM9FlaM@cluster0.0xtlgy3.mongodb.net/paytmapp")
+
+const userSchema=new mongoose.Schema({
+    firstName:String,
+    lastName:String,
+    username:String,
+    password:String
+})
+
+const accountSchema=new mongoose.Schema({
+    userId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:"true"},
+    balance:{type:Number,required:true}
+})
+
+const User=mongoose.model('User',userSchema);
+
+const Account=mongoose.model('Account',accountSchema);
+
+module.exports={
+    User,
+    Account
+}
